a35173c0d83acea3fef3cc23f7dcb9c751598e50
“Best <bestnyah7@gmail.com>
2022-11-06 21:51:22 +0100

    add api status dep

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 app = FastAPI()
 
 
-@app.get("/", status_code=200)
+@app.get("/", status_code=status.HTTP_200_OK)
 def get_endpoint():
     return {"get" : "endpoint"}
